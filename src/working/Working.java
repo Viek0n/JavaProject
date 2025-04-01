cb06830786ec4d675eb06f411da3732d639add6e
DoTrongTin <tintrongdo162@gmail.com>
2025-04-01 16:36:27 +0700

    Update JDK23

--- src/working/Working.java
+++ src/working/Working.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package working;
+import DAL.ConnectToSQLServer;
 
 /**
  *
@@ -14,7 +15,7 @@ public class Working {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new ConnectToSQLServer();
     }
     
 }
