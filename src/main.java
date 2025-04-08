fe15526fd84e498684ad1e7aeb31161e0ac3d8e0
Viek0n <156826575+Viek0n@users.noreply.github.com>
2025-04-08 15:34:25 +0700

    success

--- src/main.java
+++ src/main.java
@@ -1,5 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class main {
     public static void main(String[] args) {
-        
+        // Thông tin kết nối
+        String url = "jdbc:mysql://localhost:3306/qlthitracnghiem";
+        String username = "root";
+        String password = ""; // Mặc định XAMPP không có mật khẩu
+
+        try {
+            // Kết nối đến cơ sở dữ liệu
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Kết nối thành công đến MySQL!");
+            // Đóng kết nối
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Kết nối thất bại!");
+            e.printStackTrace();
+        }
     }
 }
