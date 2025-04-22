2c71995dcb658c89cc8e0483c1212b0c7528a219
Viek0n <156826575+Viek0n@users.noreply.github.com>
2025-04-22 15:58:18 +0700

    TEST

--- src/GUI/Ulti.java
+++
@@ -1,8 +0,0 @@
-package GUI;
-import java.awt.Color;
-
-public class Ulti {
-    public static Color MainColor = new Color(134,157,218);
-    public static Color BananaLeaf = new Color(85,251,168,100);
-    public static Color GrayinBox = new Color(217,217,217,100);
-}
