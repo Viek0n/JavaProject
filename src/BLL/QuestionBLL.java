71bfafe78b21b4e56aebf882e8c2d33b806e5757
Viek0n <156826575+Viek0n@users.noreply.github.com>
2025-04-22 15:54:31 +0700

    Update QuestionBLL.java

--- src/BLL/QuestionBLL.java
+++ src/BLL/QuestionBLL.java
@@ -1,22 +1,33 @@
 package BLL;
 
 import DAL.QuestionDAL;
+import DTO.AnswerDTO;
 import DTO.QuestionDTO;
-
+import java.util.ArrayList;
 public class QuestionBLL {
     //Valid check
-    
+    private static Boolean checkAns(ArrayList<AnswerDTO> ans){
+        Boolean valid = false;
+        for(AnswerDTO a: ans)
+            if(a.getRight())
+                valid = true;
+        return valid;
+    }
     //Get
     public static QuestionDTO get(String ID){
         return QuestionDAL.getByID(ID);
     }
     //Insert
     public static Boolean add(QuestionDTO quest){
-        return QuestionDAL.add(quest);
+        if(checkAns(quest.getAns()))
+            return QuestionDAL.add(quest);
+        return false;
     }
     //Update
     public static Boolean update(QuestionDTO quest){
-        return QuestionDAL.update(quest);
+        if(checkAns(quest.getAns()))
+            return QuestionDAL.update(quest);
+        return false;
     }
     //Delete
     public static Boolean delete(String ID){
