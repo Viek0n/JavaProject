731a903f8b56cc88c66ddc1974af50eda3b00e5c
DoTrongTin <tintrongdo162@gmail.com>
2025-04-24 10:48:13 +0700

    a

--- src/DAL/QuestionDAL.java
+++ src/DAL/QuestionDAL.java
@@ -15,16 +15,18 @@ import java.util.ArrayList;
 public class QuestionDAL {
      public static ArrayList<QuestionDTO> getAll(){
         ArrayList<QuestionDTO> array = new ArrayList<>();
-        String sql = "SELECT * FROM nhomquyen";
-        try(Connection conn = DriverManager.getConnection(Connect.url, Connect.user, Connect.pass);
+        String sql = "SELECT * FROM cauhoi";
+        try(Connection conn = Connect.getConnection();
             PreparedStatement stmt = conn.prepareStatement(sql)) {
             ResultSet rs = stmt.executeQuery();
             while(rs.next()){
-                array.add(new QuestionDTO(rs.getString("MaCH"),
-                        rs.getString("MaChuong"),
-                        rs.getString("MaND"),
-                        Enums.DifficultValue.valueOf(rs.getString("DoKho")),
-                        rs.getString("NoiDung")));
+                QuestionDTO ques = new QuestionDTO(rs.getString("MaCH"),
+                                                ChapterDAL.get(rs.getString("MaChuong")),
+                                                rs.getString("MaND"),
+                                                Enums.DifficultValue.valueOf(rs.getString("DoKho")),
+                                                rs.getString("NoiDung"));
+                                                ques.setAns(AnswerDAL.getAllByQId(ques.getID()));
+                array.add(ques);
             }
         }catch (SQLException e) {
             System.out.println("Kết nối cauhoi thất bại!");
@@ -42,13 +44,12 @@ public class QuestionDAL {
                 ResultSet rs = stmt.executeQuery();
                 if(rs.next()){
                     QuestionDTO newQues = new QuestionDTO(rs.getString("MaCH"),
-                                                        rs.getString("MaChuong"),
+                                                        ChapterDAL.get(rs.getString("MaChuong")),
                                                         rs.getString("MaND"),
                                                         Enums.DifficultValue.valueOf(rs.getString("DoKho")),
                                                         rs.getString("NoiDung"));
                     newQues.setAns(AnswerDAL.getAllByQId(newQues.getID()));
-                    newQues.setChapter(MiscDAL.getChapter(newQues.getChapterID()));
-                    newQues.setCourse(MiscDAL.getCourseByChapID(newQues.getChapterID()));
+                    newQues.setSubject(SubjectDAL.getByChapID(ID));
                     return newQues;
                 }
             } catch(SQLException e){
@@ -99,7 +100,7 @@ public class QuestionDAL {
     public static Boolean add(QuestionDTO a){
         String sql = "INSERT INTO cauhoi (MaCH, NoiDung, DoKho, MaChuong, MaND) VALUES (?, ?, ?, ?, ?)";
         try (Connection conn = DriverManager.getConnection(Connect.url, Connect.user, Connect.pass);
-            PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
             a.setID(getNextId());
             stmt.setString(1, a.getID());
             stmt.setString(2, a.getText());
--- src/DAL/SubjectDAL.java
+++ src/DAL/SubjectDAL.java
@@ -1,14 +1,14 @@
 package DAL;
 
+import DTO.SubjectDTO;
+import MICS.Connect;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import MICS.Connect;
 public class SubjectDAL {
-    public static String getCourseByChapID(String ID){
+    public static SubjectDTO getByChapID(String ID){
         String sql = "SELECT chuong.MaChuong, chuong.TenChuong, monhoc.TenMH FROM chuong LEFT JOIN monhoc ON chuong.MonHoc = monhoc.MaMH WHERE MaChuong = ?";
         try(Connection conn = DriverManager.getConnection(Connect.url, Connect.user, Connect.pass);
         PreparedStatement stmt = conn.prepareStatement(sql)){
@@ -16,11 +16,11 @@ public class SubjectDAL {
             stmt.setString(1,ID);
             ResultSet rs = stmt.executeQuery();
             if(rs.next())
-                return rs.getString("TenMH");
+                return new SubjectDTO(rs.getString("MaMH"), rs.getString("TenMH"));
         }catch(SQLException e){
             System.out.println("Kết nối monhoc thất bại!");
             e.printStackTrace();
         }
-        return "";
+        return null;
     }
 }
